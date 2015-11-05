a3473c682f6f9e077cf034400e0f06b6e7f91bb7
tm34451 <tm34451@gmail.com>
2015-11-05 08:03:28 -0600

    Initial commit

--- fb.js
+++ fb.js
@@ -1,16 +1,16 @@
-
-$(document).ready(function(){
-	for(i = 1; i < 101; i++){
-
+function prompter() {
+	var num = parseInt(prompt("Enter a number", ""));	//prompt for a number
+	
+	//loop through user input num and flow of 'if' statements to display game
+	for(i = 1; i <= num; i++){
 		if(i % 3 == 0 && i % 5 == 0){
-			document.write("fizzBuzz" + "<br>");
+			document.write("FizzBuzz" + "<br>");
 		}else if(i % 3 == 0){
-			document.write("fizz" + "<br>");
+			document.write("Fizz" + "<br>");
 		}else if(i % 5 == 0){
-			document.write("buzz" + "<br>");
+			document.write("Buzz" + "<br>");
 		}else{
 			document.write(i + "<br>");
-		};
-
-	};
-});
+		}
+	}
+}
